10ce4acaccffedaf3c33780ba11bca2c53b33d1c
okodelabraham6-crypto <okodelabraham6@gmail.com>
2026-03-25 21:21:17 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,23 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+ /*Name: Okodel Abraham*/ 
+ /*Student Number: 24/U/BIE/11014/PE*/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main() {
 
+    float amount, transaction_fee,fee_amount, total_amount_deducted;
+
+    printf(" please enter amount to send: ");
+    scanf("%f", &amount);
+
+    printf("please enter the  transaction fee   (%%): ");
+    scanf("%f", &transaction_fee);
+    fee_amount = (transaction_fee / 100) * amount;
+    total_amount_deducted = amount + fee_amount;
+
+    printf("\n--- transaction history ---\n");
+    printf("Fee Amount: %.2f\n", fee_amount);
+    printf("Total Amount Deducted: %.2f\n", total_amount_deducted);
+system("pause");
     return 0;
 }
