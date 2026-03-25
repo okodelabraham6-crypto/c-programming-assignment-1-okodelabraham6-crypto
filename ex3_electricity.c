1f4bc9803d3d2303ed809113652066302e2fee16
okodelabraham6-crypto <okodelabraham6@gmail.com>
2026-03-25 22:04:02 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,24 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/*Name: Okodel Abraham*/
+/*reg number : 24/U/BIE/11014/PE*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main(){
+
+    float units_consumed, cost_per_unit, the_final_bill;
+
+    printf("Enter units consumed: ");
+    scanf("%f", &units_consumed);
+
+    printf("Enter cost per unit: ");
+    scanf("%f", &cost_per_unit);
+
+    the_final_bill = units_consumed * cost_per_unit;
+
+    printf("\nTotal bill: %.2f UGX\n", the_final_bill);
+
+    system("pause");
 
     return 0;
 }
