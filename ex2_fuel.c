f68542a6956cc08ed452688d8ea4e57c19a178f9
okodelabraham6-crypto <okodelabraham6@gmail.com>
2026-03-25 21:49:39 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,22 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+/*Name : okodel abraham*/
+/*reg number : 24/U/BIE/11014/PE*/
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main(){
+   
+     float distance_travelled;
+     float fuel_used;
+     float Efficiency;
+    printf("Enter distance travelled (KM): ");
+    scanf("%f", &distance_travelled);
+
+    printf("Enter the number of litres : ");
+    scanf("%f", &fuel_used);
+    Efficiency = distance_travelled / fuel_used;
+    printf("Fuel efficiency: %.2f km/litre\n", Efficiency);
+
+system("pause");
 
     return 0;
 }
